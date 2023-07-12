484ac1a461614c3a019afa84e873412c5ee03988
lhk3337 <lhk3337@naver.com>
2023-07-12 17:33:47 +0900

    [FEAT]  new meetup page component add

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+const NewMeetupPage = () => {
+  const addMeetupHandler = (enteredMeetupData) => {};
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+};
+
+export default NewMeetupPage;
