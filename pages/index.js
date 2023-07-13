01a9ac1fb9eec000103adf4db84508606ef4e129
lhk3337 <lhk3337@naver.com>
2023-07-13 23:14:28 +0900

    [FEAT] SSR(getServerSideProps)로 설정하기

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,23 @@ export default function Home({ meetups }) {
     </>
   );
 }
-export async function getStaticProps() {
+// getServerSideProps은 req, res를 사용 가능하다.
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
-    revalidate: 10,
   };
 }
+
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 10,
+//   };
+// }
