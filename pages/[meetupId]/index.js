f1ad39c94ddb180048da71ae8320a1e44b5bad0a
lhk3337 <lhk3337@naver.com>
2023-07-14 21:12:22 +0900

    [FEAT] detail page SSG류 설정하기

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,18 +2,36 @@ import React from "react";
 import { DUMMY_MEETUPS } from "../index";
 
 import MeetupDetail from "../../components/meetups/MeetupDetail";
-import { useRouter } from "next/router";
-const MeetupDetails = () => {
-  const router = useRouter();
-  const { meetupId } = router.query;
-
-  const DETAIL_DUMMY_DATA = DUMMY_MEETUPS.find((value) => value.id === meetupId);
+// import { useRouter } from "next/router";
+const MeetupDetails = ({ meetupData }) => {
+  // const router = useRouter();
+  // const { meetupId } = router.query;
+  // const DETAIL_DUMMY_DATA = DUMMY_MEETUPS.find((value) => value.id === meetupId);
 
   return (
     <div>
-      <MeetupDetail {...DETAIL_DUMMY_DATA} />
+      <MeetupDetail {...meetupData} />
     </div>
   );
 };
 
 export default MeetupDetails;
+
+export async function getStaticPaths() {
+  const paths = DUMMY_MEETUPS.map((event) => ({ params: { meetupId: event.id } }));
+  return {
+    fallback: false,
+    paths,
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  const DETAIL_DUMMY_DATA = DUMMY_MEETUPS.find((value) => value.id === meetupId);
+  return {
+    props: {
+      meetupData: DETAIL_DUMMY_DATA,
+    },
+  };
+}
