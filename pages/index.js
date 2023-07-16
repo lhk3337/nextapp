6c97ec34136c34e19b768ff03468be10b3526fbd
lhk3337 <lhk3337@naver.com>
2023-07-16 21:04:13 +0900

    [FEAT] mongodb에서 all data api 설정 및 메인 페이지 데이터 패칭

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
-export const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 5, 12356 Some City",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 5, 12356 Some City",
-    description: "This is a second meetup!",
-  },
-];
+
 export default function Home({ meetups }) {
   return (
     <>
@@ -23,22 +8,27 @@ export default function Home({ meetups }) {
   );
 }
 // getServerSideProps은 req, res를 사용 가능하다.
-export async function getServerSideProps(context) {
-  const req = context.req;
-  const res = context.res;
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
 
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-  };
-}
-
-// export async function getStaticProps() {
 //   return {
 //     props: {
 //       meetups: DUMMY_MEETUPS,
 //     },
-//     revalidate: 10,
 //   };
 // }
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3000/api/meetup", {
+    method: "GET",
+  });
+  const meetups = await response.json();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({ ...meetup })),
+    },
+    revalidate: 1,
+  };
+}
