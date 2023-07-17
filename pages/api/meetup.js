d5501b79c8b06e48f504edfd73ac5fce71981ca6
lhk3337 <lhk3337@naver.com>
2023-07-17 15:52:05 +0900

    [FIX] meetup list api mongo close 설정하기

--- pages/api/meetup.js
+++ pages/api/meetup.js
@@ -2,5 +2,6 @@ import { closeMongoDB, collectionMongoDB } from "../../libs/mongodb";
 async function handler(req, res) {
   const meetupCollect = await (await collectionMongoDB("meetups")).find().toArray();
   res.status(200).json(meetupCollect);
+  await closeMongoDB();
 }
 export default handler;
